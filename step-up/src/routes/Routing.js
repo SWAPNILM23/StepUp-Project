08a55bbc64a78b62bfc3d9240416360c292b88af
ShubhamA18 <shubhamaswale55@gmail.com>
2024-02-14 20:11:45 +0530

    Signin Page added

--- step-up/src/routes/Routing.js
+++ step-up/src/routes/Routing.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
-import Signup from '../components/signup/Signup';
 import NotFound from '../components/NotFound/NotFound';
 import Home from '../components/Home';
+import Signin from '../signin/Signin';
 
 const Routing = () => {
     return (
@@ -12,7 +12,7 @@ const Routing = () => {
             <Header />
             <Routes>
                 <Route path='/' element={<Home/>}></Route>
-                <Route path='/signup' element={<Signup/>}></Route>
+                <Route path='/signin' element={<Signin/>}></Route>
                 <Route path='*' element={<NotFound/>}></Route>
             </Routes>
             <Footer />
