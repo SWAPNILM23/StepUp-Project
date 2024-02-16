f22a5710a04c595be2ccd0aed767d60add047742
ShubhamA18 <shubhamaswale55@gmail.com>
2024-02-16 16:05:54 +0530

    signup file added

--- step-up/src/components/signup/Signup.js
+++ step-up/src/components/signup/Signup.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Signup = () => {
     return (
-        <div>
-            SignUp
+        <div className='signup-box'>
+            <h3>Sign up</h3>
+            <form>
+                <div className='register-box'>
+                    
+                </div>
+            </form>
         </div>
     )
 }
